b83b1d7d70911f08d0b0813bd312fdafc3cce88b
root <s5f14iulus69w@hotmail.com>
2024-03-12 23:09:27 -0300

    Iniciando la rama Development

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ app = express()
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/index.html')
-  res.sendFile(__dirname + '/index.css')
+  res.sendFile(__dirname + '/home/index.html')
+  res.sendFile(__dirname + '/home/index.css')
   res.sendFile(__dirname + '/index.js')
 })
 
 app.listen(3000, function() {
-  console.log('Example app listening on port 3000!')
+  console.log('Listening on port 3000')
 })
